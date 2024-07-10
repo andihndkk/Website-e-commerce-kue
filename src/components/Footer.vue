ccb1234366212d8062d3a404de2fd385478988f0
serverbeit <151661368+andihndkk@users.noreply.github.com>
2024-07-10 12:06:35 +0700

    menambah fitur

---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer class="bg-[#F1ECE6] ">
+    <div class="mx-auto  w-full max-w-screen-xl  py-6 lg:py-8 container ">
+      <div class="flex justify-between">
+        <div class="mb-6 md:mb-0 mr-16 ">
+          <a>
+            <img src="/public/img/CakekuLogo.png" class="h-12" alt="Flowbite Logo" />
+          </a>
+          <p class="pt-2 ">
+            Rasakan kelezatan setiap gigitan CAKKEU Macaroon. Dibuat dengan bahan berkualitas tinggi
+            <div> untuk Anda, setiap saat</div>
+           
+          </p>
+        </div>
+        <div class="grid grid-cols-2 sm:grid-cols-2">
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Home</h2>
+            <ul class="text-gray-500">
+              <li class="mb-1">
+                <a href="https://flowbite.com/" class="hover:underline">Product</a>
+              </li>
+              <li class="mb-1">
+                <a href="https://tailwindcss.com/" class="hover:underline ">About Us</a>
+              </li>
+              <li class="mb-1">
+                <a href="https://tailwindcss.com/" class="hover:underline">Contact</a>
+                <li>
+                <a href="https://tailwindcss.com/" class="hover:underline">Keranjang</a>
+              </li>
+              </li>
+            </ul>
+          </div>
+          <div class="ml-8">
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Contact</h2>
+            <ul class="text-gray-500">
+              <li class="mb-4">
+                <a href="#" class="hover:underline">Jl. Tambakang No.2, RT.03/RW.01, Kedung Waringin, Kec. Tanah Sereal, Kota Bogor, Jawa Barat 16164 </a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">
+                    <p>Telp.081377021417</p>
+                    <p>Email: cakkeucake@gmail.com</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/TestimonialStar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-[#F1ECE6] w-[300px] rounded rounded-lg px-10 py-10 w-[408px] h-[276px]">
+    <div class="flex">
+      <img :src="picture" class="w-24 h-24" />
+      <div class="px-2 py-2">
+        <p class="text-xl font-semibold">{{ name }}</p>
+        <div class="flex flex-row">
+          <div v-for="items in star" class="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" viewBox="0 0 24 24">
+              <path
+                fill="#FFA500"
+                d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pt-6">
+      <p>{{ comment }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  picture: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  star: {
+    type: Number,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  }
+})
+</script>
